d6d26fa4b43d85363c654c50ee38876c56235e81
unknown <rishavkmr21@gmail.com>
2022-07-20 09:04:47 +0530

    changed

---
+++ Phase1Lesson1/src/methods/callmethodtest.java
@@ -0,0 +1,11 @@
+package methods;
+
+public static main(String[] args) {
+	int x = 100;
+	int y = 100;
+
+	main test = new main();
+	//int result = test.SumTwoNum(x, y);
+	System.out.println("sum=" + result);
+
+}
