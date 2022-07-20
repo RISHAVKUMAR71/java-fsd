62349fbee794b97cfbfab97ef1669ce00af674a3
unknown <rishavkmr21@gmail.com>
2022-07-20 07:38:17 +0530

    changed

---
+++ Phase1Lesson1/src/public1/PubAccessModifier.java
@@ -0,0 +1,13 @@
+package public1;
+
+public class PubAccessModifier {
+
+	public void display() {
+		System.out.println(" Public access modifier .....");
+		int b = 99;
+		System.out.println(" Public access modifier +++++++");
+	}
+	
+}
+
+
