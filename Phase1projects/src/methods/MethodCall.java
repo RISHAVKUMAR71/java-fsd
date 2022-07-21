dc433f8fed476f0fbc18d675c756ff9611a3d9bf
unknown <rishavkmr21@gmail.com>
2022-07-21 08:19:49 +0530

    changed

---
+++ Phase1projects/src/methods/MethodCall.java
@@ -0,0 +1,20 @@
+package methods;
+
+class main {
+
+	int SumTwoNum(int a, int b) {
+		int sum = a + b;
+		return sum;
+
+	}
+
+//	public static void main(String[] args) {
+//		int x = 100;
+//		int y = 100;
+//
+//		main test = new main();
+//		int result = test.SumTwoNum(x, y);
+//		System.out.println("sum=" + result);
+//
+//	}
+}
